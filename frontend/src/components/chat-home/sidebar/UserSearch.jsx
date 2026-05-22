0f8a88e14b775629f3059d5fca2d19dbed662bc9
Benjamin Inglis <bjammin134@gmail.com>
2026-05-22 13:29:41 -0700

    Remove debug logs

--- frontend/src/components/chat-home/sidebar/UserSearch.jsx
+++ frontend/src/components/chat-home/sidebar/UserSearch.jsx
@@ -34,8 +34,6 @@ export default function UserSearch({ mode }) {
     fetchOrganizationUsersAsync();
   }, [setOrganizationUsers, selectedOrganization]);
 
-  console.log(organizationUsers);
-
   const debounceOnChange = debounce(async (e) => {
     const input = e.target.value.trim().toLowerCase();
     const filteredResults = organizationUsers.filter((usr) => {
