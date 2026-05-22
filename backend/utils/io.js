48672d7b195051ae4591b37c4e2bf3739d4ceb69
Benjamin Inglis <bjammin134@gmail.com>
2026-05-22 11:52:26 -0700

    Add cache-miss recovery to validate user permissions

--- backend/utils/io.js
+++ backend/utils/io.js
@@ -1,6 +1,8 @@
 import { Message } from '../models/messageModel.js';
 import userChannelMap from '../utils/userChannelMap.js';
 import { saveAttachmentRecords } from './multerS3.js';
+import User from '../models/userModel.js';
+import Channel from '..models/channelModel.js';
 
 export const setupIO = (io) => {
   const userSocketMap = new Map(); // socketId -> userId
@@ -125,15 +127,19 @@ const createMessage = async (messageData) => {
 
 const validateUserPermissions = (userId, channelId) => {
   if (!channelId) return true;
-  const channels = userChannelMap.data.get(userId);
+
+  let channels = userChannelMap.data.get(userId);
+
+  if (!channels){
+    // Cache miss - hydrate from DB and store
+    const user = await User.findByPk(userId, {
+      include: [{model: Channel, as: 'Channels', attributes: ['id']}],
+    })
+  if (!user) return false;
+  channels = user.Channels.map((ch) => ch.id);
+  userChannelMap.data.set(userId, channels);
+  }
+  
   return channels.includes(channelId);
 };
 
-// TODO: Figure out why members added to channel get an error trying to send a message:
-// TypeError: Cannot read properties of undefined (reading 'includes')
-//     at validateUserPermissions (file:///opt/render/project/src/backend/utils/io.js:129:19)
-//     at Socket.<anonymous> (file:///opt/render/project/src/backend/utils/io.js:45:12)
-//     at Socket.emit (node:events:518:28)
-//     at Socket.emitUntyped (/opt/render/project/src/backend/node_modules/socket.io/dist/typed-events.js:69:22)
-//     at /opt/render/project/src/backend/node_modules/socket.io/dist/socket.js:697:39
-//     at process.processTicksAndRejections (node:internal/process/task_queues:85:11)
