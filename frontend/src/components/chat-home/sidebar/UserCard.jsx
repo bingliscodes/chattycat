779a38ed6e740ae24194ac7e6ee65a6e8ed6b2a2
Benjamin Inglis <bjammin134@gmail.com>
2026-05-22 12:58:58 -0700

    Fix bug causing add user to channel button to never fire handler

--- frontend/src/components/chat-home/sidebar/UserCard.jsx
+++ frontend/src/components/chat-home/sidebar/UserCard.jsx
@@ -1,8 +1,9 @@
 import { Flex, Text } from '@chakra-ui/react';
 import { AiFillPlusCircle } from 'react-icons/ai';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 import { addUserToChannel } from '@/utils/js/apiCalls';
+import { toaster } from '@/components/ui/toaster';
 import { ChatContext } from '@/contexts/ChatContext';
 import UserAvatar from '@/components/common/Avatar';
 import { OrganizationContext } from '@/contexts/OrganizationContext';
@@ -10,10 +11,45 @@ import { OrganizationContext } from '@/contexts/OrganizationContext';
 export default function UserCard({ user, mode }) {
   const { channel, channelUsers, handleSetDirectMessage } =
     useContext(ChatContext);
+  const [error, setError] = useState(false);
 
   const { selectedOrganization } = useContext(OrganizationContext);
 
   const isInChannel = channelUsers?.includes(user.id);
+
+  async function handleAddUserToChannel(e) {
+    e.preventDefault();
+
+    const addUserPromise = addUserToChannel(
+      user.id,
+      channel.id,
+      selectedOrganization.id,
+    );
+
+    toaster.promise(addUserPromise, {
+      loading: {
+        title: 'Adding user to channel...',
+        description: 'Checking that a user with entered email exists...',
+      },
+      success: {
+        title: 'Successfully added user to your channel!',
+        description:
+          'Have them login to their ChattyCat account and go to your organization to to access the new channel.',
+      },
+      error: (err) => ({
+        title: 'Failed to add user to channel',
+        description: err.message || 'An unexpected error occured!',
+      }),
+    });
+
+    try {
+      await addUserPromise;
+      setError(false);
+    } catch (err) {
+      setError(err);
+      console.error(err);
+    }
+  }
 
   return mode === 'ch' ? (
     <Flex align="center" gap={2}>
@@ -36,9 +72,7 @@ export default function UserCard({ user, mode }) {
         <AiFillPlusCircle
           cursor="pointer"
           size="1.5rem"
-          onClick={() =>
-            addUserToChannel(user.id, channel.id, selectedOrganization.id)
-          }
+          onClick={handleAddUserToChannel}
         />
       )}
     </Flex>
